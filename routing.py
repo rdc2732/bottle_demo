5ec19d14cc5e7ddeefab4f4f5a26112d9ba775e4
Rick Cottle <richard.cottle@honeywell.com>
2019-05-17 10:58:00 -0700

    Updated with page and article indexes.

--- routing.py
+++ routing.py
@@ -8,9 +8,17 @@ def login():
 def register():
     return '<h1>On the register page</h1>'
 
+@route('/article')
+def article():
+    return '<h1>This is the article index page</h1>'
+
 @route('/article/<id>')
 def article(id):
     return '<h1>You are viewing article ' + id + '</h1>'
+
+@route('/page')
+def page():
+    return '<h1>This is the page index page</h1>'
 
 @route('/page/<id>/<name>')
 def page(id, name):
